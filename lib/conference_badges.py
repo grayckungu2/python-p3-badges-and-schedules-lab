c6a5555926d135cf3c802b4617e6e1531aa0d47d
Grace kungu <grayckungu2@gmail.com>
2023-08-22 19:25:38 +0300

    assignment complted

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,22 @@
 def badge_maker(name):
-    return None
+    return "Hello, my name is {}.".format(name)
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(badge_maker(name))
+    return badges
 
 def assign_rooms(names):
-    return None
+    rooms = []
+    for i, name in enumerate(names):
+        rooms.append("Hello, {}! You'll be assigned to room {}!".format(name, i+1))
+    return rooms
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    rooms = assign_rooms(names)
+    for badge in badges:
+        print(badge)
+    for room in rooms:
+        print(room)
